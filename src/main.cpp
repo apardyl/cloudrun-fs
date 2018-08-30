f5c0e012e2e9688dd44ed0a9b99fb8f5689140a7
Adam Pardyl <adam@pardyl.com>
2018-08-30 22:05:34 +0200

    Force foreground mode

--- src/main.cpp
+++ src/main.cpp
@@ -9,28 +9,28 @@ int main(int argc, char *argv[]) {
         printf("cloudrun-fs <filesystem meta file> <hash store path> <private store path> <remote address:remote port>\n");
     }
 
-    RemoteFSConnection remoteFSConnection(argv[4]);
-
-    HashStore store(argv[2], &remoteFSConnection);
-    filesystem::Filesystem fs;
+    auto *remoteFSConnection = new RemoteFSConnection(argv[4]);
+    auto *store = new HashStore(argv[2], remoteFSConnection);
+    auto *fs = new filesystem::Filesystem;
 
     int res = open(argv[1], O_RDONLY);
     if (res == -1) {
         printf("unable to open fs.meta\n");
         exit(1);
     }
-    if (!fs.ParseFromFileDescriptor(res)) {
+    if (!fs->ParseFromFileDescriptor(res)) {
         printf("Unable to parse fs.meta\n");
         exit(1);
     }
     close(res);
 
-    auto *hashfs = new HashFS(std::make_unique<filesystem::Filesystem>(std::move(fs)), &store);
-    CachedFileStore cachedFileStore(argv[3], &remoteFSConnection);
-    auto *cachefs = new CacheFS(&remoteFSConnection, &cachedFileStore);
+    auto *hashfs = new HashFS(std::unique_ptr<filesystem::Filesystem>(fs), store);
+    auto *cachedFileStore = new CachedFileStore(argv[3], remoteFSConnection);
+    auto *cachefs = new CacheFS(remoteFSConnection, cachedFileStore);
 
-    struct fuse_args args = FUSE_ARGS_INIT(argc, argv);
+    struct fuse_args args = FUSE_ARGS_INIT(argc - 4, argv + 4);
     fuse_opt_parse(&args, nullptr, nullptr, nullptr);
+    fuse_opt_add_arg(&args, "-f");
     fuse_opt_add_arg(&args, "-oro,allow_other,default_permissions");
 
     return hfs_main(args, hashfs, cachefs);
