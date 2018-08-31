129931ca195f7950af9ad42aecb30ca011749aec
Adam Pardyl <adam@pardyl.com>
2018-08-31 20:56:35 +0200

    Fork to background, fix #1

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <unistd.h>
 #include "fs_fuse.h"
+#include "config.h"
 
 
 int main(int argc, char *argv[]) {
@@ -9,29 +10,40 @@ int main(int argc, char *argv[]) {
         printf("cloudrun-fs <filesystem meta file> <hash store path> <private store path> <remote address:remote port>\n");
     }
 
-    auto *remoteFSConnection = new RemoteFSConnection(argv[4]);
-    auto *store = new HashStore(argv[2], remoteFSConnection);
     auto *fs = new filesystem::Filesystem;
-
     int res = open(argv[1], O_RDONLY);
     if (res == -1) {
-        printf("unable to open fs.meta\n");
+        printf("unable to open fs meta\n");
         exit(1);
     }
     if (!fs->ParseFromFileDescriptor(res)) {
-        printf("Unable to parse fs.meta\n");
+        printf("Unable to parse fs meta\n");
         exit(1);
     }
     close(res);
-
-    auto *hashfs = new HashFS(std::unique_ptr<filesystem::Filesystem>(fs), store);
-    auto *cachedFileStore = new CachedFileStore(argv[3], remoteFSConnection);
-    auto *cachefs = new CacheFS(remoteFSConnection, cachedFileStore);
 
     struct fuse_args args = FUSE_ARGS_INIT(argc - 4, argv + 4);
     fuse_opt_parse(&args, nullptr, nullptr, nullptr);
     fuse_opt_add_arg(&args, "-f");
     fuse_opt_add_arg(&args, "-oro,allow_other,default_permissions");
 
-    return hfs_main(args, hashfs, cachefs);
+    res = 0;
+    if (DEBUG == 0) {
+        res = fork();
+        if (res == 0) {
+            setsid();
+            chdir("/");
+        }
+    }
+
+    if (res == 0) {
+        auto *remoteFSConnection = new RemoteFSConnection(argv[4]);
+        auto *store = new HashStore(argv[2], remoteFSConnection);
+        auto *hashfs = new HashFS(std::unique_ptr<filesystem::Filesystem>(fs), store);
+        auto *cachedFileStore = new CachedFileStore(argv[3], remoteFSConnection);
+        auto *cachefs = new CacheFS(remoteFSConnection, cachedFileStore);
+        return hfs_main(args, hashfs, cachefs);
+    }
+
+    return 0;
 }
